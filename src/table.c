af852a98c9bbaa6f733bb2708cb3397b9f7747ac
ttibsi <ashisbitt@icloud.com>
2026-08-28 07:51:20 +0100

    Ch30

--- src/table.c
+++ src/table.c
@@ -20,7 +20,9 @@ void freeTable(Table* table) {
 }
 
 static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
-    uint32_t index = key->hash % capacity;
+    // bitwise-AND is a faster operation than modulo
+    // unit32_t index = key->hash % capacity;
+    uint32_t index = key->hash & (capacity - 1);
     Entry* tombstone = NULL;
 
     for (;;) {
@@ -37,7 +39,8 @@ static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
             return entry;
         }
 
-        index = (index + 1) % capacity;
+        // index = (index + 1) % capacity;
+        index = (index + 1) & (capacity - 1);
     }
 }
 
@@ -115,7 +118,8 @@ ObjString* tableFindString(Table* table, const char* chars,
         int length, uint32_t hash) {
     if (table->count == 0) { return NULL; }
 
-    uint32_t index = hash % table->capacity;
+    // uint32_t index = hash % table->capacity;
+    uint8_t index = hash & (table->capacity - 1);
     for (;;) {
         Entry* entry = &table->entries[index];
         if (entry->key == NULL) {
@@ -128,7 +132,8 @@ ObjString* tableFindString(Table* table, const char* chars,
             return entry->key;
         }
 
-        index = (index + 1) % table->capacity;
+        // index = (index + 1) % table->capacity;
+        index = (index + 1) & (table->capacity - 1);
     }
 }
 
